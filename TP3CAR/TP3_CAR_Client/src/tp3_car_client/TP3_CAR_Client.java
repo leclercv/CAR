ecb9ee73086ddb27db06bbf6dc6cca0d338c3ab3
Neoragorn <casiersofian@gmail.com>
2017-02-18 16:53:24 +0100

    modif tp3

--- TP3CAR/TP3_CAR_Client/src/tp3_car_client/TP3_CAR_Client.java
+++ TP3CAR/TP3_CAR_Client/src/tp3_car_client/TP3_CAR_Client.java
@@ -5,17 +5,9 @@
  */
 package tp3_car_client;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
